34363ac0a772c587ea11d6818d85b22cdef4110c
Pablo Bellido <69220256+Pablitox26@users.noreply.github.com>
2023-03-26 16:08:40 -0500

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 var port = Environment.GetEnvironmentVariable("PORT")??"8080";
+// var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 
 app.MapGet("/", () =>
 {
@@ -20,5 +20,5 @@ app.MapPost("/", () =>
     return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
 });
 
-app.Run($"http://0.0.0.0:{port}");
-
+// app.Run($"http://0.0.0.0:{port}");
+app.Run();
